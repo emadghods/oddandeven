fa4aa70627eaf29c75813c522ef2a5854e7b10b7
emadghods <emad.ghods@gmail.com>
2020-06-30 17:23:14 +0430

    new1project add shude

---
+++ src/new1project.java
@@ -0,0 +1,29 @@
+
+ import java.util.*;
+public class new1project {
+    public static void main(String arg[]) {
+        Scanner vorudi = new Scanner(System.in);
+        System.out.println(" please insert your number");
+        int a;
+        a = vorudi.nextInt();
+        if (a % 2 == 0) {
+            System.out.println(" add shoma zoj mibashad");
+        }
+            else {
+                System.out.println(" add shoma fard mibashad");
+
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
